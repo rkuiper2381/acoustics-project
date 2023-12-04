9d5ceb54d58063b4a5405c2bcd2b40e1d7149ada
Nazar99991 <lethbridgenathan@gmail.com>
2023-12-03 22:25:17 -0500

    Error fixing and making test-terminal a tad better

--- test-terminal.py
+++ test-terminal.py
@@ -7,10 +7,12 @@ test1 = Model(file1)
 print("Sample Rate: " + str(test1.sample_rate))
 print("Length: " + str(test1.get_duration()) + " seconds")
 test1.plot_waveform()
+test1.plot_freqs()
 
 print("Test 2: " + str(file2))
 test2 = Model(file2)
 print("Sample Rate: " + str(test2.sample_rate))
 print("Length: " + str(test2.get_duration()) + " seconds")
 test2.plot_waveform()
-test.plot_freqs()
+test2.plot_freqs()
+
