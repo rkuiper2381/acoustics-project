193dcfa6efe8ad1a199233ab01b021fc7ed37f2a
Nazar99991 <lethbridgenathan@gmail.com>
2023-12-03 22:00:28 -0500

    Small changes for improved readability and testing

--- test-terminal.py
+++ test-terminal.py
@@ -1,11 +1,15 @@
 from model import Model
 import matplotlib.pyplot as plt
+file1 = "testConversion.mp3"
+file2 = "test2.wav"
+print("Test 1: " + str(file1))
+test1 = Model(file1)
+print("Sample Rate: " + str(test1.sample_rate))
+print("Length: " + str(test1.get_duration()) + " seconds")
+test1.plot_waveform()
 
-test = Model("testConversion.mp3")
-test2 = Model("test2.wav")
-print(test.sample_rate)
-print(test.get_duration())
-print(test2.sample_rate)
-print(test2.get_duration())
-test.plot_waveform()
+print("Test 2: " + str(file2))
+test2 = Model(file2)
+print("Sample Rate: " + str(test2.sample_rate))
+print("Length: " + str(test2.get_duration()) + " seconds")
 test2.plot_waveform()
