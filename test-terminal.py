1c1027d60c29719487bb03ae7a9a5025e89b619c
Nott A Tseuddo <rkuiper2381@floridapoly.edu>
2023-12-03 20:22:57 -0500

    Model can now calculate duration of wav file and plot the waveform

--- test-terminal.py
+++ test-terminal.py
@@ -1,4 +1,9 @@
 from model import Model
+import matplotlib.pyplot as plt
 
 test = Model("test.wav")
+test2 = Model("test2.wav")
 print(test.sample_rate)
+print(test.get_duration())
+test.plot_waveform()
+test2.plot_waveform()
