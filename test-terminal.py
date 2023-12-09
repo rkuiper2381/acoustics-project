ae1f69246fff6fcea98a510b249fed0e05f6eaaf
Nott A Tseuddo <rkuiper2381@floridapoly.edu>
2023-12-09 17:51:08 -0500

    Calculate Resonance and Combined Rverb Plot (model.py)

--- test-terminal.py
+++ test-terminal.py
@@ -7,21 +7,28 @@ print("Test 1: " + str(file1))
 test1 = Model(file1)
 print("Sample Rate: " + str(test1.sample_rate))
 print("Length: " + str(test1.get_duration()) + " seconds")
-test1.plot_waveform()
-test1.plot_freqs()
+
+test1_fig, test1_ax = Model.create_empty_plot()
+test1.plot_waveform(test1_ax)
+test1.plot_freqs(test1_ax)
+
 
 print("Test 2: " + str(file2))
 test2 = Model(file2)
 print("Sample Rate: " + str(test2.sample_rate))
 print("Length: " + str(test2.get_duration()) + " seconds")
-test2.plot_waveform()
-test2.plot_freqs()
+
+test2_fig, test2_ax = Model.create_empty_plot()
+test2.plot_waveform(test2_ax)
+test2.plot_freqs(test2_ax)
 
 
 print("Test 3: " + str(file3))
 test3 = Model(file3)
 print("Sample Rate: " + str(test3.sample_rate))
 print("Length: " + str(test3.get_duration()) + " seconds")
-test3.plot_waveform()
-test3.plot_freqs()
+
+test3_fig, test3_ax = Model.create_empty_plot()
+test3.plot_waveform(test3_ax)
+test3.plot_freqs(test3_ax)
 
